10ac7cdc4009ab52993dcc87f6f91cef00561e64
Tushar <tushar.goel.dav@gmail.com>
2020-01-24 22:49:42 +0545

    all done
    Signed-off-by: Tushar <tushar.goel.dav@gmail.com>

--- scripts/sponsor.js
+++ scripts/sponsor.js
@@ -2,8 +2,8 @@ let powereddata=[
   {
     Name: "Grog",
     for: "Powered By",
-    Image: "./assets/sponsor/grog.png"
-
+    Image: "./assets/sponsor/grog.png",
+    link:'./'
   },
    {
     Name: "",
@@ -18,39 +18,43 @@ let powereddata=[
   {
     Name: "KTM",
     for: "Associate Partner",
-    Image: "./assets/sponsor/KTM.png"
+    Image: "./assets/sponsor/KTM.png",
+    link:'https://www.ktm.com/'
   }
 ]
 
 let sponsordata =  [
   {
-    Name: "Youth Incorporated",
-    for: "Youth Media Partner",
-    Image: "./assets/sponsors/Logo.png"
+    Name: "Woosh",
+    for: "Fashion Partner",
+    Image: "./assets/sponsor/WhooshBlack.png",
+    link:"https://whoosh.com/"
   },
   {
     Name: "PrepBytes",
     for: "Technical Partner",
-    Image: "./assets/sponsors/PrepBytes.png"
+    Image: "./assets/sponsor/prepbytes (2).png",
+    link:'https://www.prepbytes.com/'
 
   },
    {
-    Name: "Youth Incorporated",
-    for: "Youth Media Partner",
-    Image: "./assets/sponsors/Logo.png"
-  },
-  {
-    Name: "Fiesto.live",
-    for: "Media Partner",
-    Image: "./assets/sponsors/Fiesto.png"
-
-  },
-  {
-    Name: "Datatrained",
+    Name: "Data Trained",
     for: "Training Partner",
-    Image: "./assets/sponsors/atatrained.png"
+    Image: "./assets/sponsor/atatrained.png",
+    link:"https://www.datatrained.com/"
+  },
+  {
+    Name: "Anytime Fitness",
+    for: "Gyming Partner",
+    Image: "./assets/sponsor/AnytimeFitnessLogo.png",
+    link:'https://www.anytimefitness.com/'
+  },
+  // {
+  //   Name: "IAS Academy",
+  //   for: "Education Partner",
+  //   Image: "./assets/sponsor/atatrained.png"
 
-  }
+  // }
 
 ]
 
@@ -67,8 +71,8 @@ text =
 <a href="${sponsordata[i].link}"><img class="sponsorimage" src="${sponsordata[i].Image}" alt=""></a>
 </div>
 <div class="teamDetails">
-  <h5><b>${sponsordata[i].Name}</b></h5>
-  <h5>${sponsordata[i].for}</h5>
+  <h5><b>${sponsordata[i].for}</b></h5>
+  <h5>${sponsordata[i].Name}</h5>
 </div>
 </div> 
 `
@@ -92,8 +96,8 @@ for(var i=0;i<powereddata.length;i++){
   <a href="${powereddata[i].link}"><img class="sponsorimage" src="${powereddata[i].Image}" alt=""></a>
   </div>
   <div class="teamDetails">
-    <h5><b>${powereddata[i].Name}</b></h5>
-    <h5>${powereddata[i].for}</h5>
+    <h5><b>${powereddata[i].for}</b></h5>
+    <h5>${powereddata[i].Name}</h5>
   </div>
   </div> 
   `
